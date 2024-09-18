405ddf4453ece67c0c5dc9cd53751266e7ca3560
João Victor de Melo Bezerra <joaovicmelobez@gmail.com>
2024-09-18 16:44:50 -0300

    feat: adding the counter and exceptions

---
+++ sintaxe_basica_com_java/ps-validation/src/Contador.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.lang.Throwable;
+
+public class Contador {
+    public static void main(String[] args) {
+        int numberOne = 0;
+        int numberTwo = 0;
+        try {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Put the first number:");
+            numberOne = scan.nextInt();
+            System.out.println("Put the second number");
+            numberTwo = scan.nextInt();
+
+        } catch (Exception e) {
+            System.out.println("Data type mismatch");
+        }
+
+        try{
+            contar(numberOne, numberTwo);
+        } catch(ParametrosInvalidosException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static void contar(int numberOne, int numberTwo) throws ParametrosInvalidosException{
+        if(numberOne > numberTwo){
+            throw new ParametrosInvalidosException("The first parameter is bigger than the second one");
+        }
+
+        int contagem = numberTwo - numberOne;
+        for(int count = 0; count <= contagem; count++){
+            System.out.println(count);
+        }
+    }
+}
